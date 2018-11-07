ffa930e23609b8f81d0f7b2c02499e47765c4fbc
eyee19 <eyee305@gmail.com>
2018-11-07 11:35:35 -0800

    sanitizing input for only positive numbers

--- numbers.sh
+++ numbers.sh
@@ -9,13 +9,17 @@ EVEN=" Even"
 echo "Enter a positive number: "
 read NUMBER
 
-echo ""
-echo "Output:"
-while [ $NUMBER -ge $COUNTER ]; do
-	if [ $(($COUNTER%2)) -eq 0 ]; then
-		echo "$COUNTER$EVEN"
-	else
-		echo "$COUNTER$ODD"
-	fi
-	((COUNTER++))
-done
+if [ $NUMBER -lt 1 ]; then
+	echo "Error: number must be 1 or more"
+else
+	echo ""
+	echo "Output:"
+	while [ $NUMBER -ge $COUNTER ]; do
+		if [ $(($COUNTER%2)) -eq 0 ]; then
+			echo "$COUNTER$EVEN"
+		else
+			echo "$COUNTER$ODD"
+		fi
+		((COUNTER++))
+	done
+fi
